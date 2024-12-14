59e42fedaf99c38d39b93be0e0f732c4d637edf1
Wiviart <Wisiviart@gmail.com>
2024-12-14 23:55:14 +0700

    add effects

--- Assets/Scripts/Input UI/Click.cs
+++ Assets/Scripts/Input UI/Click.cs
@@ -4,7 +4,8 @@ using UnityEngine.EventSystems;
 
 public class Click : MonoBehaviour, IPointerDownHandler
 {
-    [SerializeField] private ParticleSystem effect;
+    [SerializeField] private ParticleSystem squareEffect;
+    [SerializeField] private ParticleSystem starEffect;
     [SerializeField] private Animator anim;
     private const string DESTROY = "Destroy";
     [SerializeField] private Note note;
@@ -14,22 +15,27 @@ public class Click : MonoBehaviour, IPointerDownHandler
     {
         if (isClicked) return;
         isClicked = true;
-        
+
         note.Stop();
 
         int score = ScoreChecker.ScoreByPosition(transform);
+        if (score == 3) SpawnEffect(starEffect);
         Observer.OnClickTrigger(score);
 
         StartCoroutine(PlayAnimationAndDestroy());
     }
 
-    private IEnumerator PlayAnimationAndDestroy()
+    private void SpawnEffect(ParticleSystem effect, Transform parent = null)
     {
         var pos = transform.position;
         pos.z = 10;
-        var effectInstance = Instantiate(
-            effect, pos, Quaternion.identity, transform);
-        effectInstance.Play();
+        var vfx = Instantiate(effect, pos, Quaternion.identity, parent);
+        vfx.Play();
+    }
+
+    private IEnumerator PlayAnimationAndDestroy()
+    {
+        SpawnEffect(squareEffect, transform);
 
         var animHandler = new AnimatorHandler(anim);
         animHandler.PlayAnimation(DESTROY);
